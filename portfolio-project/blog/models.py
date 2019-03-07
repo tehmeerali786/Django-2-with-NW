feea5e7269a34b447e50797a45f5df4e12f1fb01
Tehmeer Ali Paryani <freelanceali786@gmail.com>
2019-03-07 21:55:58 +0500

    2nd last commit on 2nd

--- portfolio-project/blog/models.py
+++ portfolio-project/blog/models.py
@@ -9,8 +9,17 @@ class Blog(models.Model):
 
 
 
+    def __str__(self):
+        return self.title
 
 
+
+    def summary(self):
+        return self.body[:100]
+
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
 
 
 # Add the Blog app to settings
